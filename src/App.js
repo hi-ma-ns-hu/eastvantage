3d27d11dfe3b6d64c77e4a8fba39572479da0f62
Himanshu <hk80726@gmail.com>
2022-07-03 11:33:43 +0530

    eastvantage assignment created

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [randomUser, setRandomUser] = useState(null)
+
+  const fetchRandomUser = async () => {
+    let user = await axios.get('https://randomuser.me/api')
+    setRandomUser(user.data.results[0])
+    localStorage.setItem('randUser', JSON.stringify(user.data.results[0]))
+    console.log(localStorage.getItem('randUser'))
+    return user
+  }
+
+  useEffect(() => {
+    ; (async () => {
+      await fetchRandomUser()
+    })()
+  }, [])
+
+  const handleClick = () => {
+    localStorage.clear()
+    ;(async () => {
+      await fetchRandomUser()
+    })()
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {!randomUser ? 'Loading...' : <span><p>Name: {randomUser.name.title} {randomUser.name.first} {randomUser.name.last}</p>
+        <p>Email: {randomUser.email}</p>
+      </span>}
+      <button onClick={handleClick}>Refresh</button>
+    </>
   );
 }
 
